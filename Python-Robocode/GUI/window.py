6d44ce12b1161c82ae9ee26d4913d3b080cc8233
Almog Yalinewich <almog.yalin@gmail.com>
2025-02-16 23:23:26 +0200

    update code

--- Python-Robocode/GUI/window.py
+++ Python-Robocode/GUI/window.py
@@ -78,7 +78,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.graphicsView.setScene(self.scene)
         self.scene.AddRobots(self.botList)
         self.timer.timeout.connect(self.scene.advance)
-        self.timer.start((self.horizontalSlider.value()**2)/100.0)
+        self.timer.start()
+        self.timer.setInterval(self.horizontalSlider.value()**2//100)
         self.resizeEvent()
     
     @pyqtSlot(int)
@@ -86,7 +87,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         Slot documentation goes here.
         """
-        self.timer.setInterval((value**2)/100.0)
+        self.timer.setInterval(value**2//100)
     
     @pyqtSlot()
     def on_actionNew_triggered(self):
